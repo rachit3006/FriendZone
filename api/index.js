51362d10a1f2bce9c20204b2d4e049a4045b65f5
rachit3006 <arachit3006@gmail.com>
2023-12-12 18:42:01 +0530

    removed nginx

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://192.168.208.1:3050"
+    origin: "http://localhost:3000"
   })
 );
 app.use(cookieParser());
